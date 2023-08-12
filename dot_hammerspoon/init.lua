9c6c24bb7ee819254b27865cfa9881a76c93add6
rxvt <17208811+rxvt@users.noreply.github.com>
2023-08-12 17:52:06 +1000

    Update kitty shortcut to use shift-ctrl-space

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -1,5 +1,5 @@
 -- Based on https://github.com/kovidgoyal/kitty/issues/45#issuecomment-568920629
-hs.hotkey.bind({"ctrl"}, "space", function()
+hs.hotkey.bind({"ctrl", "shift"}, "space", function()
     local app = hs.application.get("kitty")
     if app then
         app:selectMenuItem({"Shell", "New OS Window"})
