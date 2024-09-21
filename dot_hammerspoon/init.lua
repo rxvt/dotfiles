5c4e92fa370bb1158cef6d700947babf69dc0191
Shane Anderson <17208811+rxvt@users.noreply.github.com>
2024-09-21 15:53:49 +1000

    Remove hammerspoon config

--- dot_hammerspoon/init.lua
+++
@@ -1,9 +0,0 @@
--- Based on https://github.com/kovidgoyal/kitty/issues/45#issuecomment-568920629
-hs.hotkey.bind({"ctrl", "shift"}, "space", function()
-    local app = hs.application.get("kitty")
-    if app then
-        app:selectMenuItem({"Shell", "New OS Window"})
-    else
-        hs.application.launchOrFocus("kitty")
-    end
-end)
