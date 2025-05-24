0b63e6f0be160503158ce6a39e9aa3a6f4a8bc7a
Shane Anderson <17208811+rxvt@users.noreply.github.com>
2025-05-24 10:57:30 +1000

    Add custom blink config

---
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,31 @@
+-- https://github.com/LazyVim/LazyVim/discussions/5186
+return {
+	"saghen/blink.cmp",
+	-- Make blink.cmp toogleable
+	opts = function(_, opts)
+		vim.b.completion = true
+
+		Snacks.toggle({
+			name = "Completion",
+			get = function()
+				return vim.b.completion
+			end,
+			set = function(state)
+				vim.b.completion = state
+			end,
+		}):map("<leader>uk")
+
+		opts.enabled = function()
+			return vim.b.completion ~= false
+		end
+
+		-- Disable preselect and autocomplete
+		opts.completion = opts.completion or {}
+		opts.completion.list = opts.completion.list or {}
+		opts.completion.list.selection = {
+			preselect = true, -- Do not preselect
+			auto_insert = false, -- Do not auto insert
+		}
+		return opts
+	end,
+}
