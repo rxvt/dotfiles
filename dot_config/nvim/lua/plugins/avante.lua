1309105af25cd530a0924c9d20e3c3dd396ed843
Shane Anderson <17208811+rxvt@users.noreply.github.com>
2025-06-07 20:03:48 +1000

    Update model

--- dot_config/nvim/lua/plugins/avante.lua
+++ dot_config/nvim/lua/plugins/avante.lua
@@ -19,7 +19,7 @@ return {
       },
       claude = {
         endpoint = "https://api.anthropic.com",
-        model = "claude-sonnet-4-20250514",
+        model = "claude-3-5-sonnet-20241022",
         extra_request_body = {
           temperature = 0.75,
           max_tokens = 4096,
