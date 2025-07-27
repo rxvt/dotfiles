cc2683b894a1f8ce71310fd91b54ca9527478ccd
Shane Anderson <17208811+rxvt@users.noreply.github.com>
2025-07-27 16:23:13 +1000

    Add toggle for basedpyright LSP

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,17 @@ vim.keymap.set("v", "<", "<gv", { silent = true })
 vim.keymap.set("v", ">", ">gv", { silent = true })
 vim.keymap.set("n", "<C-]>", "<C-i>", { desc = "Jump forward in jumplist" })
 vim.keymap.set("n", "<C-[>", "<C-o>", { desc = "Jump backward in jumplist" })
+
+-- Toggle basepyright type checker
+vim.keymap.set("n", "<leader>cL", function()
+  local clients = vim.lsp.get_clients({ name = "basedpyright" })
+  if #clients > 0 then
+    for _, client in ipairs(clients) do
+      vim.lsp.stop_client(client.id)
+    end
+    vim.notify("Basedpyright disabled")
+  else
+    require("lspconfig").basedpyright.setup({})
+    vim.notify("Basedpyright enabled")
+  end
+end, { desc = "Toggle Basedpyright" })
