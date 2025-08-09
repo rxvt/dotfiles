f9ac057dc8420d99db9964b6beaa76dbcfbb58f1
Shane Anderson <17208811+rxvt@users.noreply.github.com>
2025-08-09 15:18:03 +1000

    Add list of Mason packages I always want

---
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,33 @@
+-- In your Mason plugin configuration
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      -- LSP servers
+      "basedpyright",
+      "bash-language-server",
+      "debugpy",
+      "delve",
+      "docker-compose-language-service",
+      "dockerfile-language-server",
+      "emmet-language-server",
+      "emmet-ls",
+      "gofumpt",
+      "goimports",
+      "gopls",
+      "json-lsp",
+      "lua-language-server",
+      "markdown-toc",
+      "markdownlint-cli2",
+      "marksman",
+      "ruff",
+      "shellcheck",
+      "shfmt",
+      "sqlfluff",
+      "stylua",
+      "taplo",
+      "yaml-language-server",
+      -- Add all your essential tools here
+    },
+  },
+}
