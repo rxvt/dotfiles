e2f6c9831293f8fbe40715b54c2889e939b9be1f
Shane Anderson <17208811+rxvt@users.noreply.github.com>
2025-11-07 13:22:41 +1100

    Move back to basedpyright

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,5 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.nord_italic = false
-vim.g.lazyvim_python_lsp = "ty"
 vim.o.scrolloff = 5
+vim.g.lazyvim_python_lsp = "basedpyright"
