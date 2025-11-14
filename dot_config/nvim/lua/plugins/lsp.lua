dc6507cc37dd95e206f2138111b1f1781201c1b7
Shane Anderson <17208811+rxvt@users.noreply.github.com>
2025-11-14 11:20:46 +1100

    Add basedpyright toggle shortcut back in

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -4,5 +4,27 @@ return {
     opts = {
       inlay_hints = { enabled = false },
     },
+    keys = {
+      {
+        "<leader>cL",
+        function()
+          local bufnr = vim.api.nvim_get_current_buf()
+          local clients = vim.lsp.get_clients({ bufnr = bufnr, name = "basedpyright" })
+
+          if #clients > 0 then
+            -- Stop basedpyright
+            for _, client in ipairs(clients) do
+              vim.lsp.stop_client(client.id)
+            end
+            vim.notify("Basedpyright stopped", vim.log.levels.INFO)
+          else
+            -- Start basedpyright
+            vim.cmd("LspStart basedpyright")
+            vim.notify("Basedpyright started", vim.log.levels.INFO)
+          end
+        end,
+        desc = "Toggle basedpyright LSP",
+      },
+    },
   },
 }
