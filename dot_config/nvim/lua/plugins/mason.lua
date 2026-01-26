c269c7d0ffdcf27cc857455dc0bc62cde8a2f29a
Shane Anderson <17208811+rxvt@users.noreply.github.com>
2026-01-26 17:27:28 +1100

    Update mason

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -1,34 +1,34 @@
 return {
-  "mason-org/mason.nvim",
-  opts = function(_, opts)
-    opts.ui = opts.ui or {}
-    opts.ui.border = "rounded"
+	"mason-org/mason.nvim",
+	opts = function(_, opts)
+		opts.ui = opts.ui or {}
+		opts.ui.border = "rounded"
 
-    opts.ensure_installed = opts.ensure_installed or {}
-    vim.list_extend(opts.ensure_installed, {
-      -- LSP servers
-      "basedpyright",
-      "bash-language-server",
-      "debugpy",
-      "delve",
-      "docker-compose-language-service",
-      "dockerfile-language-server",
-      "emmet-language-server",
-      "emmet-ls",
-      "gofumpt",
-      "goimports",
-      "gopls",
-      "json-lsp",
-      "lua-language-server",
-      "markdown-toc",
-      "markdownlint-cli2",
-      "marksman",
-      "ruff",
-      "shellcheck",
-      "shfmt",
-      "stylua",
-      "taplo",
-      "yaml-language-server",
-    })
-  end,
+		opts.ensure_installed = opts.ensure_installed or {}
+		vim.list_extend(opts.ensure_installed, {
+			-- LSP servers
+			"bash-language-server",
+			"debugpy",
+			"delve",
+			"docker-compose-language-service",
+			"dockerfile-language-server",
+			"emmet-language-server",
+			"emmet-ls",
+			"gofumpt",
+			"goimports",
+			"gopls",
+			"json-lsp",
+			"lua-language-server",
+			"markdown-toc",
+			"markdownlint-cli2",
+			"marksman",
+			"ruff",
+			"shellcheck",
+			"shfmt",
+			"stylua",
+			"taplo",
+			"ty",
+			"yaml-language-server",
+		})
+	end,
 }
