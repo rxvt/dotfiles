cc340debcf011fadada7040572df9a63c2504b26
Shane Anderson <17208811+rxvt@users.noreply.github.com>
2026-01-26 17:30:13 +1100

    Change LSP to ty

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,4 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.o.scrolloff = 5
-vim.g.lazyvim_python_lsp = "basedpyright"
+vim.g.lazyvim_python_lsp = "ty"
