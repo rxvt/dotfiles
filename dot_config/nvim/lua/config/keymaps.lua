0e0e4ee2aed709cf02af26b38106b1f7e754ceb3
Shane Anderson <17208811+rxvt@users.noreply.github.com>
2026-04-30 12:06:22 +1000

    Fix Markdown whichkey group
    Markdown group now only appears if the buffer contains a markdown file

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -9,4 +9,23 @@ vim.keymap.set("n", "<C-]>", "<C-i>", { desc = "Jump forward in jumplist" })
 vim.keymap.set("n", "<C-[>", "<C-o>", { desc = "Jump backward in jumplist" })
 vim.keymap.set("n", "<leader>q", "q", { noremap = true, silent = true })
 vim.keymap.set("n", "q", "<Nop>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>ms", "I~~<ESC>A~~<ESC>", { desc = "Markdown Strikethrough Line" })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown" },
+  callback = function(event)
+    local map = function(lhs, rhs, desc)
+      vim.keymap.set("n", lhs, rhs, { desc = desc, buffer = event.buf })
+    end
+
+    map("<leader>ms", "I~~<ESC>A~~<ESC>", "Markdown Strikethrough Line")
+    -- map("<leader>mb", ..., "Markdown Bold")
+    -- etc.
+
+    local ok, wk = pcall(require, "which-key")
+    if ok then
+      wk.add({
+        { "<leader>m", group = "markdown", icon = "󰍔", buffer = event.buf },
+      })
+    end
+  end,
+})
